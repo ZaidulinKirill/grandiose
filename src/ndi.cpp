9b946d04bb81f850ffb359f58fb2e3d733dc1bf3
Kirill <zaidulinkirill@gmail.com>
2021-09-03 13:26:41 +0300

    test

--- src/ndi.cpp
+++ src/ndi.cpp
@@ -7,12 +7,14 @@ using namespace Napi;
 
 class FindSourcesWorker : public AsyncWorker {
     public:
-        FindSourcesWorker(Napi::Env env, Napi::Promise::Deferred& deferred, double waitTime, double iterations, const char * extraIPs)
-        : AsyncWorker(env), env(env), deferred(deferred), waitTime(waitTime), iterations(iterations), extraIPs(extraIPs) {}
+        FindSourcesWorker(Napi::Promise::Deferred& deferred, double waitTime, double iterations, const char * extraIPs)
+        : AsyncWorker(Env()), deferred(deferred), waitTime(waitTime), iterations(iterations), extraIPs(extraIPs) {}
 
         ~FindSourcesWorker() {}
 
     void Execute() override {
+      Napi::Env env = Env();
+
       NDIlib_find_create_t find_create;
       find_create.show_local_sources = true;
       find_create.p_groups = NULL;
@@ -54,9 +56,12 @@ class FindSourcesWorker : public AsyncWorker {
       return;
     }
 
+    void OnOK() override {
+      HandleScope scope(Env());
+    }
+
     private:
         Napi::Promise::Deferred deferred;
-        Napi::Env env;
         double waitTime; 
         double iterations; 
         const char * extraIPs;
@@ -76,7 +81,7 @@ Napi::Promise FindMethod(const Napi::CallbackInfo& info) {
   double iterations = info[1].As<Napi::Number>().DoubleValue();
   const char * extraIPs = info.Length() == 3 ? info[2].As<Napi::String>().Utf8Value().c_str() : NULL;
 
-  FindSourcesWorker* worker = new FindSourcesWorker(env, deferred, waitTime, iterations, extraIPs);
+  FindSourcesWorker* worker = new FindSourcesWorker(deferred, waitTime, iterations, extraIPs);
   worker->Queue();
   return deferred.Promise();
 }
